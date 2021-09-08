e1c6e63a38c2403f48f95b72f98deb0b8d800972
sgalway2086 <sgalway2086@gmail.com>
2021-09-08 00:33:23 +0000

    add requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask
-import pymongo
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
@@ -16,5 +14,3 @@ if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-hello()
