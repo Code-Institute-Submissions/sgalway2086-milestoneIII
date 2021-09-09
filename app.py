78de3954bb5e460368912f7e9807a5682c0efed7
sgalway2086 <sgalway2086@gmail.com>
2021-09-09 03:35:15 +0000

    change to camel case

--- app.py
+++ app.py
@@ -16,29 +16,33 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    stringforchange = mongo.db.recipes.find_one()["steps"]
+    stepsStringForChange = mongo.db.recipes.find_one()["steps"]
     finished = False
-    unpackedstepsstring = []
+    unpackedStepsString = []
     while finished == False:
-        num1 = stringforchange.find('{space}')
+        num1 = stepsStringForChange.find('{space}')
         num2 = num1 + 7
-        instruction = stringforchange[0:num1]
-        remove = stringforchange[0:num2]
-        unpackedstepsstring.append(instruction)
-        print(unpackedstepsstring)
-        stringforchange = stringforchange.replace(remove, "")
-        print(stringforchange)
-        if len(stringforchange) == 0:
-            return render_template("recipes.html", recipes=recipes,  
-            unpackedstepsstring = unpackedstepsstring)
+        instruction = stepsStringForChange[0:num1]
+        remove = stepsStringForChange[0:num2]
+        unpackedStepsString.append(instruction)
+        stepsStringForChange = stepsStringForChange.replace(remove, "")
+        if len(stepsStringForChange) == 0:
+            finished == True
+            return render_template("recipes.html", recipes=recipes,
+                unpackedStepsString = unpackedStepsString)
         else:
             continue
+
+
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
