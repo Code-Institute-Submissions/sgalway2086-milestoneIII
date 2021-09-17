e4411db5964c0afc0eb0dfa27e072b06017be92e
sgalway2086 <sgalway2086@gmail.com>
2021-09-17 07:26:57 +0000

    change code slightly

--- app.py
+++ app.py
@@ -333,4 +333,5 @@ def test():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT"))
+            port=int(os.environ.get("PORT")),
+            debug=False)
